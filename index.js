66be8dc01cb6b2e06be69e7de771a47c224f418d
Linda Foo <linda.foo.dev@gmail.com>
2018-04-19 18:16:50 -0700

    modify cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const {dbConnect} = require('./db-mongoose');
 
 mongoose.Promise = global.Promise;
 
-const { PORT, DATABASE_URL } = require('./config');
+const { PORT, DATABASE_URL, CLIENT_ORIGIN } = require('./config');
 
 const app = express();
 
@@ -19,15 +19,11 @@ const app = express();
 app.use(morgan('common'));
 
 // CORS
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-	if (req.method === 'OPTIONS') {
-		return res.sendStatus(204);
-	}
-	next();
-});
+app.use(
+	cors({
+			origin: CLIENT_ORIGIN
+	})
+);
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
